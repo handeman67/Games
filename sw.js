2e00cb15e1d4e01aa13537f4ce939a0054ca14ce
handeman67 <steve1@who.net>
2025-11-24 19:28:19 -0700

    added PWA script

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',(e)=>{e.waitUntil(caches.open('v1').then((cache)=>{return cache.addAll(['https://handeman67.github.io/Games/']);}));});self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then((response)=>{return response||fetch(e.request);}));});
